734b44d16719d9b0794d3cffa41b5fd1fae3a7f0
Arshad Rahim <rahimarshad667@gmail.com>
2025-09-13 09:47:28 +0530

    base Url issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://task-management-iota-dusky.vercel.app/api",
+  baseURL: "https://task-management-ffe9.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
